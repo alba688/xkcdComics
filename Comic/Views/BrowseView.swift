6052d5f856062bda08613fee1b718edcae31f590
Alexandra Baker <baker.alexandra88@gmail.com>
2023-04-15 11:28:05 +0200

    AsyncImage and buttons to navigate forwards and backwards

--- Comic/Views/BrowseView.swift
+++ Comic/Views/BrowseView.swift
@@ -12,32 +12,62 @@ struct BrowseView: View {
     @State var comicInt = Int.random(in: 1...2762)
     
     var body: some View {
-        VStack {
-            
-            // Comic Area
-            Text("Title: \(viewModel.comic.title)")
-            Text("Comic Nr: \(viewModel.comic.num)")
-            Image("")
-                .frame(width: 300, height: 200)
-                .background(Color.gray)
-            
-            
-            // Navigation Buttons
-            HStack {
-                Button("<< Previous") {
-                    //action
-                }.buttonStyle(.bordered)
-                Button("Next >>") {
-                    //action
-                }.buttonStyle(.bordered)
-            }
-            
-            // Search Area
-            Text("Search comics")
-                .padding(20)
-                .border(Color.blue, width:2)
-        }.onAppear {
-            viewModel.fetchComic(comicNum: comicInt)
+        NavigationView {
+            VStack {
+                
+                // Comic Area
+                Text("\(viewModel.comic.title)")
+                    .font(.title2)
+                Text("# \(viewModel.comic.num)")
+                    .font(.headline)
+                
+                // MARK: Link to DetailView
+                Text("Explain this >>")
+                
+                
+                AsyncImage(url: URL(string: "\(viewModel.comic.img)")) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if phase.error != nil {
+                        Text("Error loading the image.")
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 350, height: 300)
+                .padding(.bottom, 10.0)
+                
+                
+                // Navigation Buttons
+                HStack {
+                
+                    Button {
+                        comicInt -= 1
+                        viewModel.fetchComic(comicNum: comicInt)
+                    } label: {
+                        Image(systemName: "arrow.left")
+                        Text("Previous")
+                    }.buttonStyle(.bordered)
+                    Spacer()
+                    Button {
+                        comicInt += 1
+                        viewModel.fetchComic(comicNum: comicInt)
+                    } label: {
+                        Text("Next")
+                        Image(systemName: "arrow.right")
+                    }.buttonStyle(.bordered)
+                }
+                .padding([.leading, .bottom, .trailing], 50.0)
+                
+                // Search Area
+                Text("Search comics")
+                    .padding(20)
+                    .border(Color.blue, width:2)
+            }.onAppear {
+                viewModel.fetchComic(comicNum: comicInt)
+            }.navigationTitle("XKCD Comics")
         }
     }
 }
