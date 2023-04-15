26c814b7bf10484f1e1783e3dd9996fd34bedeaa
Alexandra Baker <baker.alexandra88@gmail.com>
2023-04-15 10:08:51 +0200

    Initial Commit

---
+++ Comic/ComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicApp.swift
+//  Comic
+//
+//  Created by Alexandra Baker on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
